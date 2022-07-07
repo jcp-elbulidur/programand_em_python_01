e6938d4bcfcb547728bd89d13873d61ef5f617f2
Julio Pereira <juliocezar.doulo@gmail.com>
2022-07-07 19:05:20 -0300

    feat: aula_04

---
+++ aula_04/main.py
@@ -0,0 +1,86 @@
+curso = {
+    "nome": "Programando em python",
+    "conteudos": [
+        "Conceitos básicos da linguagem",
+        "Biblioteca-padrão",
+        "Coleções",
+        "Estruturas de controle",
+        "Funções e módulos",
+        "Parâmetros default, variáveis e nomeados",
+        "Programação funcional",
+        "Classes e Objetos",
+        "Herança e Polimorfismo",
+        "Armazenamento de dados em arquivos",
+        "Acesso a banco de dados",
+        "Manipulação de API Rest",
+        "Testes unitários"
+                  ],
+    "instrutor": {
+        "Nome": "Júlio Pereira",
+        "Contato": "33 99154-2651"
+    },
+    "alunos": [
+        "Aldair Ribeiro",
+        "André de Castro"
+        "Danilo Pereira",
+        "Elton Lima",
+        "Fabio Ferraz",
+        "Gideão Sudario",
+        "Jefferson André",
+        "Jeova Vieira",
+        "Lucas Pires",
+        "Marcelo São Pedro",
+        "Meggy Mendes",
+        "Michael Rochumback",
+        "Nilza Ferreira",
+        "Paulo Sergio"
+    ]
+}
+
+# ========= USANDO  APENAS METHODOS, IF OU LIST COMPREENSHIONS
+
+# CRIAR OS CONJUNTOS
+
+# 1 - Lista de todas as keys do curso
+
+# 2 - Lista de tuplas para criar 5 relacionamento entre os alunos
+
+# 3 - Lista com os nomes dos alunos
+
+# 4 - Lista com os nomes dos alunos MOSTRANDO APENAS AS TRÊS PRIMEIRAS LETRAS
+"""
+DICA
+texto = 'Julio'
+print(texto[1:4]) # Imprime uli
+
+"""
+# 5 - Lista de conteudos que COMECA COM A LETRA A
+
+# ========= USANDO APENAS METHODOS, IF OU WHILE
+
+conteudo_while = []
+
+# 1 - Preencher o conjunto conteudo_while com uma lista de dicionarios dos conteudos conforme o exemplo:
+# Exemplo: [{nome: "Conceitos básicos da linguagem", active: True}]
+
+# ========= USANDO APENAS METHODOS, IF OU FOR
+
+curso_for = {}
+alunos_for = []
+conteudos_for = []
+instrutor_for = {}
+
+# 1 - preenche a coleção curso_for com os dados do curso e acrecenta o atributo active com valor true
+
+
+# 2 - preenche a coleção alunos_for com os nomes dos alunos.
+
+# 3 - preenche a coleção conteudos_for com os dados dos conteudos.
+
+# 4 - preenche a coleção instrutor_for com os dados dos instrutor e acrecentar os meios de contatos
+
+# 5 - faça uma busca na coleção curso_for e imprime apenas os dados do instrutor
+
+# 6 - faça uma busca na coleção alunos e imprime o aluno com index 5
+
+# 7 - Faça uma leitura na coleção conteudos_for ALTERANDO todos conteudos a partir do index 6 para "NÃO APLICADO"
